f087525ea31650086005910d8677ec0eee4431bb
Ludy <1@astra-g.org>
2021-08-29 14:50:36 +0200

    v0.0.12

--- custom_components/astra_germany_wort_des_tages/sensor.py
+++ custom_components/astra_germany_wort_des_tages/sensor.py
@@ -17,7 +17,6 @@ from .const import (
     DEFAULT_NAME,
     ISSUE_URL,
     ATTR_WDT_WORD_FREQUENCY,
-    ATTR_WDT_LAST_UPDATED,
     ATTR_WDT_SPELLING,
     ATTR_WDT_MEANING,
     ATTR_WDT_ORIGIN,
@@ -150,7 +149,6 @@ class WDTSensor(Entity):
         """Return the state attributes"""
         return {
             ATTR_WDT_WORD_FREQUENCY: self._word_frequency,
-            ATTR_WDT_LAST_UPDATED: self._last_updated,
             ATTR_WDT_SPELLING: self._spelling,
             ATTR_WDT_MEANING: self._meaning,
             ATTR_WDT_ORIGIN: self._origin,
