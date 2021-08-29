f602107aa9c05a1c94ffb0a9b19d8006f204d3fc
Ludy <1@astra-g.org>
2021-08-29 12:28:36 +0200

    Create const.py

---
+++ custom_components/astra_germany_wort_des_tages/const.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+""" Constants """
+# Base component constants
+__version__ = '0.0.1'
+DOMAIN = 'wort_des_tages'
+ISSUE_URL = "https://github.com/Ludy87/astra_germany_wort_des_tages/issues"
+
+CONF_DATE = datetime.now().date()
+CONF_NAME = 'WDT'
+
+ATTR_WDT_WORD_FREQUENCY = 'wordfrequency'
+ATTR_WDT_SPELLING = 'spelling'
+ATTR_WDT_MEANING = 'meaning'
+ATTR_WDT_ORIGIN = 'origin'
+ATTR_WDT_WORD = 'word'
