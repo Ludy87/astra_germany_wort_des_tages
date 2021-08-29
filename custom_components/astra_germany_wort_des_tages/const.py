9666da25a5bf1ca93e5ceba18ce8f1ad3db13090
Ludy <1@astra-g.org>
2021-08-29 15:55:59 +0200

    v0.0.17

--- custom_components/astra_germany_wort_des_tages/const.py
+++ custom_components/astra_germany_wort_des_tages/const.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 """ Constants """
 # Base component constants
-__version__ = '0.0.16'
+__version__ = '0.0.17'
 VERSION = __version__
 NAME = "Wort des Tages"
 DOMAIN = 'wort_des_tages'
@@ -24,7 +24,7 @@ SENSOR = "sensor"
 PLATFORMS = [SENSOR]
 
 # Defaults
-DEFAULT_NAME = DOMAIN
+DEFAULT_NAME = 'WDT'
 
 STARTUP_MESSAGE = f"""
 -------------------------------------------------------------------
