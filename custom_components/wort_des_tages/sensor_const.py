18da94156dfe8d0bbb4802635327e21c82a8c336
Ludy87 <Ludy87@users.noreply.github.com>
2022-05-28 00:11:11 +0200

    Update sensor_const.py

--- custom_components/wort_des_tages/sensor_const.py
+++ custom_components/wort_des_tages/sensor_const.py
@@ -1,39 +1,50 @@
-from .const import *
+from __future__ import annotations
+
+from custom_components.wort_des_tages.const import (
+    ATTR_WDT_CURRENT_TIME,
+    ATTR_WDT_LAST_UPDATED,
+    ATTR_WDT_MEANING,
+    ATTR_WDT_ORIGIN,
+    ATTR_WDT_SPELLING,
+    ATTR_WDT_WORD,
+    ATTR_WDT_WORD_FREQUENCY,
+)
+
 
 SENSOR_TYPES = {
     ATTR_WDT_WORD: [  # state
-        'Wort des Tages',
-        'mdi:book-open-variant',
+        "Wort des Tages",
+        "mdi:book-open-variant",
         None,
     ],
     ATTR_WDT_ORIGIN: [  # origin
-        'Herkunft',
-        'mdi:map-marker-star-outline',
+        "Herkunft",
+        "mdi:map-marker-star-outline",
         None,
     ],
     ATTR_WDT_MEANING: [  # meaning
-        'Bedeutung',
-        'mdi:script-text-play-outline',
+        "Bedeutung",
+        "mdi:script-text-play-outline",
         None,
     ],
     ATTR_WDT_SPELLING: [  # spelling
-        'Worttrennung',
-        'mdi:format-text-wrapping-clip',
+        "Worttrennung",
+        "mdi:format-text-wrapping-clip",
         None,
     ],
     ATTR_WDT_WORD_FREQUENCY: [  # word_frequency
-        'Häufigkeit',
-        'hass:eye',
+        "Häufigkeit",
+        "hass:eye",
         None,
     ],
     ATTR_WDT_LAST_UPDATED: [  # last_updated
-        'Update Datum',
-        'mdi:update',
+        "Update Datum",
+        "mdi:update",
         None,
     ],
     ATTR_WDT_CURRENT_TIME: [  # current_time
-        'Update Zeit',
-        'mdi:update',
+        "Update Zeit",
+        "mdi:update",
         None,
     ],
 }
