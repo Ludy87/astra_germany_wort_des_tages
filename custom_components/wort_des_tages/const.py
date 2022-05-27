965340e229ce571ff2929143679531f295f329f2
Ludy87 <Ludy87@users.noreply.github.com>
2022-05-28 00:11:39 +0200

    Update const.py

--- custom_components/wort_des_tages/const.py
+++ custom_components/wort_des_tages/const.py
@@ -1,25 +1,25 @@
 """ Constants """
 # Base component constants
-__version__ = '0.0.27'
+__version__ = "0.0.27"
 VERSION = __version__
 NAME = "Wort des Tages"
-DOMAIN = 'wort_des_tages'
+DOMAIN = "wort_des_tages"
 ISSUE_URL = "https://github.com/Ludy87/astra_germany_wort_des_tages/issues"
 
-CONF_DATE = 'date'
-CONF_NAME = 'name'
+CONF_DATE = "date"
+CONF_NAME = "name"
 
-ATTR_WDT_WORD_FREQUENCY = 'word_frequency'
-ATTR_WDT_CURRENT_TIME = 'current_time'
-ATTR_WDT_LAST_UPDATED = 'last_updated'
-ATTR_WDT_SPELLING = 'spelling'
-ATTR_WDT_MEANING = 'meaning'
-ATTR_WDT_ORIGIN = 'origin'
-ATTR_WDT_WORD = 'word'
+ATTR_WDT_WORD_FREQUENCY = "word_frequency"
+ATTR_WDT_CURRENT_TIME = "current_time"
+ATTR_WDT_LAST_UPDATED = "last_updated"
+ATTR_WDT_SPELLING = "spelling"
+ATTR_WDT_MEANING = "meaning"
+ATTR_WDT_ORIGIN = "origin"
+ATTR_WDT_WORD = "word"
 
 # Platforms
 SENSOR = "sensor"
 PLATFORMS = [SENSOR]
 
 # Defaults
-DEFAULT_NAME = 'WDT'
+DEFAULT_NAME = "WDT"
